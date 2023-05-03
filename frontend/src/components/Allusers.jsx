d703e3c105d66e54d7e4cc830072d102fc18349b
Yasin051197 <yasinjamadar0511@gmail.com>
2023-05-03 19:50:28 +0530

    added getdata in alluser to fetch all data

--- frontend/src/components/Allusers.jsx
+++ frontend/src/components/Allusers.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navbar from './Navbar'
 import {Heading}from "@chakra-ui/react"
+import axios from 'axios'
+import { useState } from 'react'
+
+
+const getData=async()=>{
+    return await axios.get('https://onito-technology-server.onrender.com/users')
+}
+
 
 const Allusers = () => {
+    const [users,setUsers]=useState([])
+    useEffect(()=>{
+        getData().then((res)=>console.log(res.data))
+    })
   return (
     <div>
         <Navbar />
