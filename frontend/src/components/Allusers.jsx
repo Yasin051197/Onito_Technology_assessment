0b3f71d97f58bf6e1328e9573b594c1ca06f6e20
Yasin051197 <yasinjamadar0511@gmail.com>
2023-05-03 20:17:51 +0530

    destructured the data in table

--- frontend/src/components/Allusers.jsx
+++ frontend/src/components/Allusers.jsx
@@ -3,7 +3,7 @@ import Navbar from './Navbar'
 import {Heading}from "@chakra-ui/react"
 import axios from 'axios'
 import { useState } from 'react'
-
+import "./Allusers.css"
 
 const getData=async()=>{
     return await axios.get('https://onito-technology-server.onrender.com/users')
@@ -13,12 +13,38 @@ const getData=async()=>{
 const Allusers = () => {
     const [users,setUsers]=useState([])
     useEffect(()=>{
-        getData().then((res)=>console.log(res.data))
+        getData().then((res)=>setUsers(res.data))
     })
   return (
     <div>
         <Navbar />
         <Heading>Allusers</Heading>
+        <div id="users_container">
+        <table id="table" style={{width:"100%"}}>
+            <tr>
+               <th>Name</th>
+               <th>Age/Sex</th>
+               <th>Mobile</th>
+               <th>Address</th>
+               <th>Govt ID</th>
+               <th>Guardian Details</th>
+               <th>Nationality</th>
+            </tr>
+            {
+                users.map((user)=>(
+                    <tr key={user.id}>
+                        <td>{user.name}</td>
+                        <td>{user.DateorAge}/{user.sex}</td>
+                        <td>{user.mobile}</td>
+                        <td>{user.address}</td>
+                        <td>{user.Id}</td>
+                        <td>{user.gaurdianName}</td>
+                        <td>{user.nationality}</td>
+                    </tr>
+                ))
+            }
+</table>
+        </div>
     </div>
   )
 }
