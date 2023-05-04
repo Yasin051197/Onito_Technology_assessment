c44572bea5e778a7e63795b7a6db7232ecde6be2
Yasin051197 <yasinjamadar0511@gmail.com>
2023-05-04 11:00:52 +0530

    added Allusers functionallity

--- frontend/src/components/Allusers.jsx
+++ frontend/src/components/Allusers.jsx
@@ -1,33 +1,60 @@
 import React, { useEffect } from 'react'
 import Navbar from './Navbar'
-import {Heading}from "@chakra-ui/react"
+import {Box, Heading}from "@chakra-ui/react"
 import axios from 'axios'
 import { useState } from 'react'
 import "../Css/Allusers.css"
 
-const getData=async()=>{
-    return await axios.get(`https://onito-technology-server.onrender.com/users`)
+const getData=async(limit)=>{
+    return await axios.get(`https://onito-technology-server.onrender.com/users?_limit=${limit}`)
 }
 
 
 const Allusers = () => {
     const [users,setUsers]=useState([])
+    const [limit,setLimit]=useState(10)
+
+
+    const handleSearch=(e)=>{
+        let a=e.target.value
+        const filteredArray=users.filter(obj=>{
+            return Object.values(obj).some(value=> {
+                if(typeof value === "string"){
+                    return value.includes(a)
+                }
+                return false
+            })
+        })
+        setUsers(filteredArray)
+    }
     const handleChange=(e)=>{
+        let val=Number(e.target.value)
+        console.log(typeof(val))
+        setLimit(val)
+        getData(val).then((res)=>setUsers(res.data))
     }
     useEffect(()=>{
-        getData().then((res)=>setUsers(res.data))
-    })
+        getData(limit).then((res)=>setUsers(res.data))
+    },[])
+
   return (
     <div>
         <Navbar />
         <Heading>Allusers</Heading>
         <div id="users_container">
-            <select id="limit" onSubmit={handleChange}>
+            <Box m={'auto'} w={'90%'} display={'flex'} justifyContent={'space-evenly'}>
+            <select id="limit" onChange={handleChange}>
                     <option >limit</option>
-                    <option value={1}>1</option>
-                    <option value={2}>2</option>
-                    <option value={3}>3</option>
+                    <option value='1'>1</option>
+                    <option value='5'>5</option>
+                    <option value='10'>10</option>
+                    <option value='15'>15</option>
             </select>
+            <div><label htmlFor="input">search : </label>
+            <input type="text" placeholder='Search.....' onChange={handleSearch} /></div>
+            </Box>
+            <br />
+            <br />
         <table id="table" style={{width:"100%"}}>
             <tr>
                <th>Name</th>
